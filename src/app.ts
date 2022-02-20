a03b9c7e24c2dac2c510c45bd70b9bf6c8d12538
Göker Tanrısever <gokertanrisever@gmail.com>
2022-02-20 20:53:35 +0300

    app: helmet middleware added

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import helmet from "helmet";
 import apiRoutes from "./routes"
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
 
 app.use('/api', apiRoutes);
 
