3ca9f8b0745b327a21b594df9ffe5980df99c499
Göker Tanrısever <gokertanrisever@gmail.com>
2022-02-20 23:11:09 +0300

    server: utils export changed

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
 import app from "./app";
 import http from "http";
-import { normalizePort } from "./utils";
+import utils from "./utils";
 import logger from "./utils/logger";
 
 // Get port from environment and store in Express.
-const port = normalizePort(process.env.PORT || "3000");
+const port = utils.normalizePort(process.env.PORT || "3000");
 app.set("port", port);
 
 // Create HTTP server.
